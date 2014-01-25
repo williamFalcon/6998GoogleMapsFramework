5f31c18f45c3c5c9c5d87df7d86837a51a515eab
williamFalcon <waf2107@columbia.edu>
2014-01-24 20:49:27 -0500

    added framework

---
+++ COMSMapManager.framework/Versions/A/Headers/GoogleMapManager.h
@@ -0,0 +1,14 @@
+//
+//  GoogleMapManager.h
+//  COMSMapManager
+//
+//  Created by William Falcon on 1/24/14.
+//  Copyright (c) 2014 ColumbiaU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleMapManager : NSObject
+
+-(void)load;
+@end
