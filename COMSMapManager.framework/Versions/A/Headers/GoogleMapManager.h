4d6d48a47518473a1af49342955a5d081a1f95bf
williamFalcon <waf2107@columbia.edu>
2014-01-24 21:23:25 -0500

    outlined main search method

--- COMSMapManager.framework/Versions/A/Headers/GoogleMapManager.h
+++ COMSMapManager.framework/Versions/A/Headers/GoogleMapManager.h
@@ -7,8 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <CoreLocation/CLLocation.h>
 @interface GoogleMapManager : NSObject
 
--(void)load;
+/*
+ Returns an array of Venue Objects for a 2d lat lon, a radius (in meters) for a search query
+ The result of this query will be provided in the completion block
+ */
++(void) nearestVenuesForLatLong:(CLLocationCoordinate2D)latLong withinRadius:(double)radius forQuery:(NSString *)query googleMapsAPIKey:(NSString *)apiKey searchCompletion:(void(^)(NSMutableArray *results))completionHandler;
+
 @end
